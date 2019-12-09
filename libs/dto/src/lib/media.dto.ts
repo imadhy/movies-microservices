6b15284cd539a6e9461bbb89368d532809c90f2b
Bigeard <robin.bigeard@gmail.com>
2019-12-09 08:13:05 +0100

    fix(dto): change dto for graphql

--- libs/dto/src/lib/media.dto.ts
+++ libs/dto/src/lib/media.dto.ts
@@ -1,10 +1,21 @@
-export interface MediaDTO {
-    id: string;
-    title: string;
-    info: string;
-    created_at: Date;
-    updated_at: Date;
-    type: string;
-    length: number;
-    categorie: string;
+import { ObjectType, Field, ID } from "type-graphql";
+
+@ObjectType()
+export class MediaDTO {
+    @Field(() => ID)
+    readonly id: string;
+    @Field()
+    readonly title: string;
+    @Field()
+    readonly info: string;
+    @Field()
+    readonly created_at: Date;
+    @Field()
+    readonly updated_at: Date;
+    @Field()
+    readonly type: string;
+    @Field()
+    readonly length: number;
+    @Field()
+    readonly categorie: string;
 }
--- libs/dto/src/lib/message.dto.ts
+++ libs/dto/src/lib/message.dto.ts
@@ -1,5 +1,11 @@
-export interface MessageDTO {
-  message: string;
-  type: string;
-  status: number
+import { ObjectType, Field, Int } from "type-graphql";
+
+@ObjectType()
+export class MessageDTO {
+  @Field()
+  readonly message: string;
+  @Field()
+  readonly type: string;
+  @Field(() => Int)
+  readonly status: number
 }
