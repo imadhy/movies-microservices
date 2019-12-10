8593f6797660353116d429d44b35a8d49d06e137
Paul <paulereverend@gmail.com>
2019-12-10 14:56:31 +0100

    feat(entity): add category entity and access to it

---
+++ apps/d02/src/app/category/category.entity.ts
@@ -0,0 +1,10 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
+
+@Entity('Category')
+export class CategoryEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+}
