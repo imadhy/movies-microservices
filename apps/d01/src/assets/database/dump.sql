80aba9472c3d79cc6886c3601234cb97a1bc5875
AlexSuchot <alexandre.suchot@ynov.com>
2019-12-10 11:58:15 +0100

    feat(database) fix data

--- apps/d01/src/assets/database/dump.sql
+++ apps/d01/src/assets/database/dump.sql
@@ -32,7 +32,7 @@ INSERT INTO users (first_name, last_name, email, country, created_at, birthday, 
 INSERT INTO users (first_name, last_name, email, country, created_at, birthday, gender, admin) VALUES ('loïc', 'bouchain', 'loïc@ferarepasser.fr', 'France', '2019-12-10', '1999-09-04', 'male', false);
 INSERT INTO users (first_name, last_name, email, country, created_at, birthday, gender, admin) VALUES ('olivier', 'crochet', 'oliver@sinnoh.fr', 'France', '2019-11-02', '1998-09-04', 'homosapiens', false);
 INSERT INTO users (first_name, last_name, email, country, created_at, birthday, gender, admin) VALUES ('adrien', 'vauchard', 'adrien@vauchard.fr', 'Afrance', '2019-02-04', '2002-10-05', 'undefined', false);
-INSERT INTO users (first_name, last_name, email, country, created_at, birthday, gender, admin) VALUES ('marcéau', 'david', 'marcéau@david-david.fr', 'Arabie Saoudite', '2019-01-22', '2002-10-05', 'male', false);
+INSERT INTO users (first_name, last_name, email, country, created_at, birthday, gender, admin) VALUES ('marcéau', 'david', 'marcéau@david-david.fr', 'Algérie', '2019-01-22', '2002-10-05', 'male', false);
 INSERT INTO users (first_name, last_name, email, country, created_at, birthday, gender, admin) VALUES ('bigeard', 'bigg', 'bigerald@robinfalse.gouv', 'La montagne', '2019-15-04', '1998-04-09', 'male', false);
 INSERT INTO users (first_name, last_name, email, country, created_at, birthday, gender, admin) VALUES ('geoffrey', 'sisi', 'sisi@sisi.fr', 'Summoner rift', '2019-12-10', '1997-01-01', 'male', false);
 INSERT INTO users (first_name, last_name, email, country, created_at, birthday, gender, admin) VALUES ('elouan', 'lafrechoux', 'elouan@rammstein.de', 'hellfest', '2019-12-10', '2007-10-24', 'male', false);
