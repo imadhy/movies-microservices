eb6b68132de368a2be466a3d1f8c264baf5cf22b
HugoHUET <hugo.huet.14@gmail.com>
2019-12-10 15:59:58 +0100

    feat(app): get nb fav by media and call api

--- apps/f03/src/app/app.service.ts
+++ apps/f03/src/app/app.service.ts
@@ -1,13 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpService } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+
+  constructor(private readonly httpService: HttpService) {}
+  
   getData(): { message: string } {
+    this.callApi("https://jsonplaceholder.typicode.com/todos/1");
     return { message: 'Welcome to f03!' };
   }
 
   getNbFavByMedia(): String {
-    return '{methode : getNbFavByMedia}';
+    var data = [
+      {id_user : "1", id_film : "5"},
+      {id_user : "2", id_film : "6"},
+      {id_user : "3", id_film : "5"},
+      {id_user : "4", id_film : "6"}
+    ];
+
+    var nbFavByFilm: {id_film: string, nb_fav: number}[] = [];
+    
+    if (data != null) {
+      data.forEach(elm => {
+        let film_key: number = nbFavByFilm.findIndex(film => elm.id_film == film.id_film);
+        if (film_key !== -1)
+        {
+          nbFavByFilm[film_key].nb_fav++;
+        }
+        else
+        {
+          nbFavByFilm.push({id_film : elm.id_film, nb_fav : 1});
+        }
+      });
+    }
+
+    return JSON.stringify(nbFavByFilm);
   }
 
   getTopDixRateMediaByUser(): String {
@@ -16,5 +43,13 @@ export class AppService {
 
   getCommentByMedia(): String {
     return '{methode : getCommentByMedia}';
+  }
+
+  callApi(url : string): String{
+    var data: String = null;
+    this.httpService.get(url).subscribe(res => {
+      data = res.data;
+    });
+    return data;
   }
 }
