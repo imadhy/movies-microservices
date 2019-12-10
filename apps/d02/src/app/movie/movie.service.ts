1bfb123b68bab5eaa26526b11c0c8a3e80de7ecc
Moi <valentin.dezat@gmail.com>
2019-12-10 16:39:38 +0100

    feat(movie): add methods

--- apps/d02/src/app/movie/movie.service.ts
+++ apps/d02/src/app/movie/movie.service.ts
@@ -33,4 +33,40 @@ export class MovieService {
     await this.MovieRepository.delete({ id });
     return { delete: true };
   }
+
+  async sortMoviesByRelease() {
+    return this.MovieRepository.query('SELECT * FROM movie ORDER BY release');
+  }
+
+  async sortMoviesByUpdatedAt() {
+    return this.MovieRepository.query('SELECT * FROM movie ORDER BY updatedAt');
+  }
+
+  async sortMoviesByCreatedAt() {
+    return this.MovieRepository.query('SELECT * FROM movie ORDER BY createdAt');
+  }
+
+  async getMoviesByCategroy(name) {
+    return this.MovieRepository.query(
+      'SELECT * FROM movie, category WHERE movie.categoryId=category.id AND category.title like "name"'
+    );
+  }
+
+  async getMoviesByDirector(name) {
+    return this.MovieRepository.query(
+      'SELECT * FROM movie, person WHERE movie.director=person.id AND person.name like "name"'
+    );
+  }
+
+  async getMoviesByActor(name) {
+    return this.MovieRepository.query(
+      'SELECT * FROM movie,acting_movie, person WHERE acting_movie.person_id=person.id AND acting_movie.movie_id=movie.id AND name.person like "name"'
+    );
+  }
+
+  async getMoviesByProducer(name) {
+    return this.MovieRepository.query(
+      'SELECT * FROM movie, person WHERE movie.producer=person.id AND person.name like "name"'
+    );
+  }
 }
